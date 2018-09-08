d74c0423b873911e6483880b1bfbc69100befe11
Aleksey Ostrikov <alex303606@gmail.com>
2018-09-08 22:19:02 +0600

    Сделал добавление средний рейтинг для всех мест в списке

--- frontend/src/components/Place.js
+++ frontend/src/components/Place.js
@@ -3,16 +3,25 @@ import notFound from "src/assets/images/not-found.jpeg";
 import config from "src/config";
 import "src/styles/Place.scss";
 import PropTypes from "prop-types";
+import StarRatings from "react-star-ratings";
 
-const Place = ({title, clickHandler, image}) => {
+const Place = ({title, clickHandler, image, average: {avg}}) => {
 	let background = `url(${notFound})`;
-	
 	if (image) background = `url(${config.apiUrl}uploads/${image})`;
 	return (
 		<li className="one-place">
 			<div onClick={clickHandler} className="one-place_image"
 				 style={{background: `${background} center center / cover no-repeat`}}/>
 			<div onClick={clickHandler} className="one-place_title">{title}</div>
+			<div className="row flex flex--start">
+				<div className="rating-title">Overall:</div>
+				<StarRatings
+					rating={avg}
+					starRatedColor="red"
+					starDimension="30px"
+					starSpacing="3px"
+				/>
+			</div>
 		</li>
 	);
 };
@@ -20,6 +29,7 @@ const Place = ({title, clickHandler, image}) => {
 Place.propTypes = {
 	title: PropTypes.string.isRequired,
 	image: PropTypes.string.isRequired,
+	average: PropTypes.object.isRequired,
 	clickHandler: PropTypes.func.isRequired
 };
 
